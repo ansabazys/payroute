f7ac4d3d8d74bef52e6667aff8ae63ba9f15744d
ansabazys <ansabazys@gmail.com>
2026-02-16 17:45:11 +0530

    feat: redirecting

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await getServerSession();
 
   console.log(session);
 
-  return <div></div>;
+  if (session) {
+    redirect("/dashboard");
+  } else {
+    redirect("/login");
+  }
 }
